2334056b0676ef83c8df36605b7acccd3cee4ad8
Shreenath117 <43848309+Shreenath117@users.noreply.github.com>
2018-12-26 01:54:55 +0530

    Update server.r

--- server.r
+++ server.r
@@ -214,7 +214,7 @@ shinyServer(function(input, output) {
 	labels <- c("Positive", "Negative")
 	library(plotrix)
 	#pie(slices(), labels = labels, col=rainbow(length(labels)), main="Sentiment Analysis")
-	output$piechart<-renderPlot({pie3D(slices(), labels = labels, col=rainbow(length(labels)),explode=0.00, main="Sentiment Analysis") })
+	output$piechart<-renderPlot({pie(slices(), labels = labels, col=rainbow(length(labels)),explode=0.00, main="Sentiment Analysis") })
 				 
 ##Top trending tweets
   toptrends <- function(place)
