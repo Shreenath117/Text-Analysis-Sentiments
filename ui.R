ea1bd491ceb5984bac503b6c57e6b25cda10fd0c
Shreenath117 <43848309+Shreenath117@users.noreply.github.com>
2018-12-25 16:46:25 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -103,15 +103,9 @@ shinyUI(pageWithSidebar(
       
       tabPanel("Histogram",HTML
                ("<div><h3> Histograms graphically depict the positivity or negativity of peoples' opinion about of the hashtag
-                 </h3></div>"), plotOutput("histPos"), plotOutput("histNeg"), plotOutput("histScore")
+                 </h3></div>"), plotOutput("histScore"), plotOutput("histPos"), plotOutput("histNeg")
                ),
       
-      
-      tabPanel("Pie Chart",HTML("<div><h3>Pie Chart</h3></div>"), plotOutput("piechart"),HTML
-               ("<div><h4> A pie chart is a circular statistical graphic, which is divided into slices to illustrate the sentiment of the hashtag. In a pie chart, the arc length 
-                 of each slice (and consequently its central angle and area), is proportional to the quantity it represents.</h4></div>")
-                
-              ),
       
       tabPanel("Table",HTML( "<div><h3> Depicting sentiment in a tablular form on a scale of 5 </h3></div>"), tableOutput("tabledata"),
 			HTML ("<div><h4> The table depicts the sentiment (positive, negative or neutral) of the tweets 
